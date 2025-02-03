78e7d31cb7cfc9ecc3d7c133528accff654b4a30
Vinicius Rodrigues Cordeiro <vrc_crv@outlook.com.br>
2025-02-03 15:27:03 -0300

    Funções adicionar nomes, limpar campo e alertar campo vazio.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomesSorteados = [];
+function adicionarAmigo() {
+    let nomes = document.querySelector('input').value;
+    if (nomes == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        nomesSorteados.includes(nomes);
+        console.log(nomes);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    let nomes = document.querySelector('input');
+    nomes.value = '';
+}
+
+function
